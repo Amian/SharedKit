518d838c522f7ba8e018ae9add2446c1f18632bc
Anum <anu_s_m@hotmail.com>
2025-12-12 13:29:27 +0000

    Add debug logs for image loading

--- Sources/AppBrowser/CachedAsyncImage.swift
+++ Sources/AppBrowser/CachedAsyncImage.swift
@@ -14,10 +14,18 @@ private final class CachedImageLoader: ObservableObject {
     private var task: URLSessionDataTask?
 
     func load(url: URL?) {
-        guard let url else { return }
+        guard let url else {
+#if DEBUG
+            print("CachedImageLoader: no URL provided")
+#endif
+            return
+        }
         let key = url as NSURL
 
         if let cached = ImageCache.shared.object(forKey: key) {
+#if DEBUG
+            print("CachedImageLoader: using cached image for \(url.absoluteString)")
+#endif
             image = cached
             return
         }
@@ -25,15 +33,31 @@ private final class CachedImageLoader: ObservableObject {
         var request = URLRequest(url: url)
         request.cachePolicy = .returnCacheDataElseLoad
 
-        task = URLSession.shared.dataTask(with: request) { [weak self] data, _, _ in
+        task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
             guard let self else { return }
+            if let error {
+#if DEBUG
+                print("CachedImageLoader: failed to fetch \(url.absoluteString) - \(error.localizedDescription)")
+#endif
+                return
+            }
             if let data, let uiImage = UIImage(data: data) {
                 ImageCache.shared.setObject(uiImage, forKey: key)
+#if DEBUG
+                print("CachedImageLoader: fetched image for \(url.absoluteString) (\(data.count) bytes)")
+#endif
                 DispatchQueue.main.async {
                     self.image = uiImage
                 }
+            } else {
+#if DEBUG
+                print("CachedImageLoader: invalid image data for \(url.absoluteString)")
+#endif
             }
         }
+#if DEBUG
+        print("CachedImageLoader: fetching image from \(url.absoluteString)")
+#endif
         task?.resume()
     }
 
